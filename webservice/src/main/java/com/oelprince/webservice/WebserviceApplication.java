f04eaae023ed3d7c5f3ca21720f41087d7367e83
Omar Elprince <oelprince@gmail.com>
2019-09-21 18:51:33 -0400

    enable aws

--- webservice/src/main/java/com/oelprince/webservice/WebserviceApplication.java
+++ webservice/src/main/java/com/oelprince/webservice/WebserviceApplication.java
@@ -1,11 +1,15 @@
 package com.oelprince.webservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebserviceApplication {
-
+public class WebserviceApplication extends SpringBootServletInitializer {
+	// silence console logging
+	@Value("${logging.level.root:OFF}")
+	String message = "";
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceApplication.class, args);
 	}
